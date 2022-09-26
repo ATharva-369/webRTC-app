4745594c7916fc2b03f6295815e45e47d0054d47
ATharva-369 <wagh.atharva14@gmail.com>
2022-09-26 17:55:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express  = require('express');
+const app = express();
+const server = require('http').Server(app);
+app.set('view engine','ejs');
+app.use(express.static('public'));
+const {v4:uuidv4} = require('uuid');
+const io = require('socket.io')(server,{
+    cors:{
+        origin:'*'
+    }
+})
+const {ExpressPeerServer} = require('peer');
+const PeerServer = ExpressPeerServer(server,{debug : true});
+
+app.use('/peerjs',PeerServer);
+app.get('/',(req,res)=>{
+    // res.status(200).send('Hello, World!');
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('index',{roomID : req.params.room})
+})
+io.on('connection',(socket) => {
+    socket.on('join-room',(roomID,userID,userName)=>{
+        socket.join(roomID);
+        socket.on('message',(message)=>{       
+            io.to(roomID).emit('createMessage',message,userName)
+        } )
+    })
+})
+server.listen(3030)
